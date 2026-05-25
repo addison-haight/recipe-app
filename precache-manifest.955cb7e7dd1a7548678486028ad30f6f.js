e6bc5604f9c586d2ff4178ca9ad5b8de1d3a0dd4
Addison Haight <anhaight@gmail.com>
2026-05-25 17:46:49 -0600

    Updates

---
+++ precache-manifest.955cb7e7dd1a7548678486028ad30f6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1f41dccc9a75985405e5be22f892b74",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "958c4ee808893c8ae534",
+    "url": "/recipe-app/static/css/main.f3a15461.chunk.css"
+  },
+  {
+    "revision": "cb36ca21cd640765644c",
+    "url": "/recipe-app/static/js/2.6ea21f0b.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/recipe-app/static/js/2.6ea21f0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "958c4ee808893c8ae534",
+    "url": "/recipe-app/static/js/main.3c8ab108.chunk.js"
+  },
+  {
+    "revision": "9ab47c757d89f93c0d01",
+    "url": "/recipe-app/static/js/runtime-main.804532aa.js"
+  }
+]);
